8a4cd36cf80a96d5c91692a1e0d2bdc2664aa684
Irsa Fitrawan Rayes <irsa.rayes07@gmail.com>
2021-04-14 15:11:27 +0700

    Pertama

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { MaterialDesign } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:AdminComponent,
+    children:[
+      {
+        path:'dashboard',
+        component:DashboardComponent
+      },
+      {
+        path:'product',
+        component:ProductComponent
+      },
+      {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'/admin/dashboard' 
+      }
+]
+  }
+]
+@NgModule({
+  declarations: [
+    AdminComponent, 
+    DashboardComponent, 
+    ProductComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MaterialDesign,
+    FormsModule,
+  ]
+})
+export class AdminModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+import { MaterialDesign } from './material/material.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialDesign,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
