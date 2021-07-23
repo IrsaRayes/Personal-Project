f70f870f7a119ff21d6efecb81bb93415652f2f5
Irsa Fitrawan Rayes <irsa.rayes07@gmail.com>
2021-07-23 14:57:30 +0700

    project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAkPC9vjz-56XpDL3biOLE9S7Mq-YGmZu0",
+    authDomain: "data-siswa-c5c44.firebaseapp.com",
+    projectId: "data-siswa-c5c44",
+    storageBucket: "data-siswa-c5c44.appspot.com",
+    messagingSenderId: "793840484466",
+    appId: "1:793840484466:web:bf31bbbbf1dd0bdf8f42eb",
+    measurementId: "G-9D8F02P1EJ"
+  }
 };
